3933c1cffd6d1b02db51627cfaf1e42776aa25a5
zenzesama <zenzesama@gmail.com>
2026-06-21 12:01:10 +0000

    fixed and completed expand.c

--- expand.c
+++ expand.c
@@ -3,6 +3,24 @@
 
 #define MAX_SIZE 1000
 
+int max(int a, int b) {
+    if (a > b) {
+        return a;
+    }
+    else {
+        return b;
+    }
+}
+
+int min(int a, int b) {
+    if (a > b) {
+        return b;
+    }
+    else {
+        return a;
+    }
+}
+
 void expand(char s1[], char s2[]) {
     int s1_size = strlen(s1);
 
@@ -21,28 +39,32 @@ void expand(char s1[], char s2[]) {
         }
     }
 
-    printf("Left and right and s1_size are %d, %d, %d\n", left, right, s1_size);
+    // printf("Left and right and s1_size are %d, %d, %d\n", left, right, s1_size);
 
     int s1_itr, s2_itr;
-    s1_itr = left + 1;
+    s1_itr = left;
     for (s2_itr = 0; s2_itr < left; s2_itr++) {
         s2[s2_itr] = s1[s2_itr];
     }
     
     while (s1_itr < right) {
-        char prev = s1[s1_itr - 1];
-        char next = s1[s1_itr + 1];
-
         if (s1[s1_itr] == '-') {
-            for (char ch = prev; ch <= next; ch++) {
+            char prev = s1[s1_itr - 1];
+            char next = s1[s1_itr + 1];
+            
+            for (char ch = prev + 1; ch < next; ch++) {
                 s2[s2_itr++] = ch;
             }
+
             s1_itr++;
         }
-        s1_itr++;
+        else {
+            s2[s2_itr++] = s1[s1_itr];
+            s1_itr++;
+        }
     }
 
-    if (right != s1_size - 1) {
+    if (s1[right] != '-' || right != s1_size - 1) {
         for (int temp = right; temp < s1_size; temp++) {
             s2[s2_itr++] = s1[temp];
     
@@ -57,7 +79,7 @@ int main() {
     
     int i = 0, c = 0;
     
-    while ((c = getchar()) != EOF) {
+    while ((c = getchar()) != EOF && c != '\n') {
         s1[i++] = c;
     }
 
@@ -66,7 +88,7 @@ int main() {
     char s2[MAX_SIZE];
     
     expand(s1, s2);
-    printf("\nThis is the extention: %s\n", s2);
+    printf("This is the extention: %s\n", s2);
 
     return 0;
 }
